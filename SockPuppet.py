71cab88ec68e9d1a1c674bfdb5a7ed7d874ac8a3
Ian Marx <105711066+Marx-wrld@users.noreply.github.com>
2023-06-04 13:28:24 +0300

    Update SockPuppet.py

--- SockPuppet.py
+++ SockPuppet.py
@@ -1,1 +1,44 @@
 
+## Anonymous Sock Puppet Account
+
+##### Step 1
+
+- Come up with a persona for the sockpuppet account. e.g - a construction worker
+
+##### Step 2
+
+- Use Fake Name Generator to create a person whom you feel fits your sockpuppet persona.
+
+##### Step 3
+
+- Use This Person Does Not Exist to generate an image. Make sure you inspect the image closely and get one that doesn't have any obvious flaws, as they often do. It is worth picking up some Photoshop, GIMP, Affinity Photo or Designer, or other basic image manipulation skills to fix them and change the background of the image.
+
+##### Step 4
+
+- Get a burner phone, completely wiped and fresh. Can be any brand that will accept a Mint Mobile SIM card.
+
+##### Step 5
+
+- Get a burner credit card from Privacy.com to use for on Amazon and possible the Mint Mobile setup. They might need it to set up the account.
+
+##### Step 6
+
+- Set up a burner Amazon account. We're only going to use it once.
+
+##### Step 7
+
+- Buy two Mint Mobile SIM cards. You can find them various places online and in stores near you, but you can get two of them for $5 on Amazon. They also give you 1 week free trial with something like 100 text messages, which we're going to use. This gives you two cards for two sockpuppet accounts for only $5.
+
+##### Step 8
+- I like to use Amazon to have the card sent to an Amazon pickup box, which can be anonymous.
+
+##### Step 9
+
+- Get a VPN that you can set to the physical area in which you want your sockpuppet to "exist."
+
+##### Step 10
+
+- Set up the Mint Mobile trial account somewhere away from your home; as far as you're willing to go.
+
+
+
